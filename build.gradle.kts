7213b2b3ac77c03c64fbde5536e369e5842ecfe7
alter-ukko <alter.ukko@gmail.com>
2023-07-16 11:32:19 -0400

    kotlin 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -10,6 +10,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.0")
     implementation("org.apache.commons:commons-compress:1.21")
     implementation("com.offbytwo:docopt:0.6.0.20150202")
     implementation("org.slf4j:slf4j-simple:1.7.35")
@@ -32,10 +33,6 @@ tasks.withType<ProcessResources> {
 
 tasks.distTar {
     compression = Compression.GZIP
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
 }
 
 application {
